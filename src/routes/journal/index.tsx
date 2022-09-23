393141059520e4608f2856e3d801dfa01ad51e3f
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-23 23:55:43 +0200

    feat: Create base Journal page

---
+++ src/routes/journal/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div className="journal__container">
+      <div className="journal"></div>
+    </div>
+  );
+});
