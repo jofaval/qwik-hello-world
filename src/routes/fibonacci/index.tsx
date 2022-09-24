5e41586be969cd246b3433bc350592a5a4220a43
pepe_fabra_valverde <jofaval@outlook.com>
2022-09-25 00:20:49 +0200

    feat: Create fibonacci page

---
+++ src/routes/fibonacci/index.tsx
@@ -0,0 +1,53 @@
+import { component$, useStore } from "@builder.io/qwik";
+// import { fibonacciGenerator } from "~/helpers/fibonacci";
+
+export default component$(() => {
+  // const state = useStore({ fibonacci: 0, f: fibonacciGenerator() });
+  const state = useStore({
+    fibonacci: 1,
+    previous: 0,
+    current: 0,
+    backMessage: "",
+  });
+
+  return (
+    <>
+      <h1>Fibonacci</h1>
+
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {state.fibonacci}
+      </div>
+
+      <div className="actions">
+        <button
+          onClick$={() => {
+            state.backMessage =
+              "Sometimes in life, there's no going back, this is one of those times...";
+          }}
+        >
+          Back
+        </button>
+        <button
+          onClick$={() => {
+            state.current = state.fibonacci;
+            state.fibonacci = state.previous + state.current;
+            state.previous = state.current;
+            state.backMessage = "";
+          }}
+        >
+          Next
+        </button>
+      </div>
+
+      <div className="warning">{state.backMessage}</div>
+    </>
+  );
+});
+
+export interface MyCmpProps {
+  step: number;
+}
